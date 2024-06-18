549c18000919d9a71e8e00c1e1fb0874acbb5ccb
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-06-18 21:15:37 +0300

    adding some components(Views) in Python Desktop Application

--- deskop/LoginView/login.py
+++ deskop/LoginView/login.py
@@ -4,13 +4,14 @@ import sys
 import os
 
 # Add the parent directory to the sys.path to import main
-sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) ) )
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import main
 
-customtkinter.set_appearance_mode( "dark" )
-customtkinter.set_default_color_theme( "dark-blue" )
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
+root.title("Le Coupage")
 window_width = 500
 window_height = 350
 
@@ -19,11 +20,11 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
 # Find the center point
-center_x = int( screen_width / 2 - window_width / 2 )
-center_y = int( screen_height / 2 - window_height / 2 )
+center_x = int(screen_width / 2 - window_width / 2)
+center_y = int(screen_height / 2 - window_height / 2)
 
 # Set the position of the window to the center of the screen
-root.geometry( f'{window_width}x{window_height}+{center_x}+{center_y}' )
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 
 def login():
@@ -31,22 +32,19 @@ def login():
     main.open_dashboard()  # Call the function from main.py to open the main window
 
 
-frame = customtkinter.CTkFrame( master=root )
-frame.pack( pady=20, padx=60, fill="both", expand=True )
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel( master=frame, text="Login System", font=("Roboto", 24) )
-label.pack( pady=12, padx=10 )
+labelWelcome = customtkinter.CTkLabel(master=frame, text="Bine ati venit la Coupage", font=("Roboto", 24))
+labelWelcome.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry( master=frame, placeholder_text="Username" )
-entry1.pack( pady=12, padx=10 )
+username = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+username.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry( master=frame, placeholder_text="Password", show="*" )
-entry2.pack( pady=12, padx=10 )
+password = customtkinter.CTkEntry(master=frame, placeholder_text="Parola", show="*")
+password.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton( master=frame, text="Login", command=login )
-button.pack( pady=12, padx=10 )
-
-checkbox = customtkinter.CTkCheckBox( master=frame, text="Remember me" )
-checkbox.pack( pady=12, padx=10 )
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
 
 root.mainloop()
--- deskop/main.py
+++ deskop/main.py
@@ -1,55 +1,77 @@
-import tkinter
 import customtkinter
 
 
 def open_dashboard():
-    customtkinter.set_appearance_mode( "dark" )
-    customtkinter.set_default_color_theme( "dark-blue" )
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("dark-blue")
 
     root = customtkinter.CTk()
+    root.title("Le Coupage")
+    window_width = 1280
+    window_height = 920
+
+    # Get the screen dimension
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Find the center point
+    center_x = int(screen_width / 2 - window_width / 2)
+    center_y = int(screen_height / 2 - window_height / 2)
+
+    # Set the position of the window to the center of the screen
+    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
     # Set the window to full screen
-    root.attributes( '-fullscreen', True )
+    root.attributes('-fullscreen', True)
 
     def change_frame(new_frame):
         new_frame.tkraise()
 
     def exit_fullscreen(event=None):
-        root.attributes( '-fullscreen', False )
+        root.attributes('-fullscreen', False)
 
-    root.bind( "<Escape>", exit_fullscreen )  # Bind the Escape key to exit full-screen
+    def close_application():
+        root.destroy()
+
+    root.bind("<Escape>", exit_fullscreen)  # Bind the Escape key to exit full-screen
 
     # Main frame
-    main_frame = customtkinter.CTkFrame( master=root )
-    main_frame.pack( fill="both", expand=True )
+    main_frame = customtkinter.CTkFrame(master=root)
+    main_frame.pack(fill="both", expand=True)
 
     # Sidebar
-    sidebar_frame = customtkinter.CTkFrame( master=main_frame, width=200 )
-    sidebar_frame.pack( side="left", fill="y" )
+    sidebar_frame = customtkinter.CTkFrame(master=main_frame, width=200)
+    sidebar_frame.pack(side="left", fill="y")
 
     # Main content area
-    content_frame = customtkinter.CTkFrame( master=main_frame )
-    content_frame.pack( side="right", fill="both", expand=True )
+    content_frame = customtkinter.CTkFrame(master=main_frame)
+    content_frame.pack(side="right", fill="both", expand=True)
 
-    frame1 = customtkinter.CTkFrame( master=content_frame )
-    frame2 = customtkinter.CTkFrame( master=content_frame )
+    frameHome = customtkinter.CTkFrame(master=content_frame)
+    frameSettings = customtkinter.CTkFrame(master=content_frame)
 
-    for frame in (frame1, frame2):
-        frame.place( x=0, y=0, relwidth=1, relheight=1 )
+    for frame in (frameHome, frameSettings):
+        frame.place(x=0, y=0, relwidth=1, relheight=1)
 
-    label1 = customtkinter.CTkLabel( master=frame1, text="Dashboard Home", font=("Roboto", 24) )
-    label1.pack( pady=20, padx=20 )
+    labelhello = customtkinter.CTkLabel(master=frameHome, text="Dashboard Home", font=("Roboto", 24))
+    labelhello.pack(pady=20, padx=20)
 
-    label2 = customtkinter.CTkLabel( master=frame2, text="Settings", font=("Roboto", 24) )
-    label2.pack( pady=20, padx=20 )
+    label2 = customtkinter.CTkLabel(master=frameSettings, text="hey", font=("Roboto", 24))
+    label2.pack(pady=20, padx=20)
 
-    button1 = customtkinter.CTkButton( master=sidebar_frame, text="Home", command=lambda: change_frame( frame1 ) )
-    button1.pack( pady=10, padx=10 )
+    home = customtkinter.CTkButton(master=sidebar_frame, text="Home", command=lambda: change_frame(frameHome))
+    home.pack(pady=10, padx=10)
 
-    button2 = customtkinter.CTkButton( master=sidebar_frame, text="Settings", command=lambda: change_frame( frame2 ) )
-    button2.pack( pady=10, padx=10 )
+    settings = customtkinter.CTkButton(master=sidebar_frame, text="Settings", command=lambda: change_frame(frameSettings))
+    settings.pack(pady=10, padx=10)
 
-    change_frame( frame1 )  # Show the home frame by default
+    orders = customtkinter.CTkButton(master=sidebar_frame, text="Orders", command=lambda: change_frame(frameSettings))
+    orders.pack(pady=10, padx=10)
+
+    # close_button = customtkinter.CTkButton(master=main_frame, text="X", command=close_application, fg_color="red", hover_color="dark red")
+    # close_button.place(relx=1.0, rely=0.0, anchor="ne")
+
+    change_frame(frameHome)
 
     root.mainloop()
 
